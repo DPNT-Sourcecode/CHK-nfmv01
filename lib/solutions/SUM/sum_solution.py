0f148598ee5854183d71cd2260efbbf19e8e7c22
sbx_user1051 <sbx_user1051@169.254.8.101>
2022-05-27 11:47:55 +0000

    Fri May 27 11:47:55 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    return x + y
+    if x == type(int) and y == type(int):
+        return x + y
+    else:
+        return print('Cannot compute this sum - must be between integers between 0-100')
 
