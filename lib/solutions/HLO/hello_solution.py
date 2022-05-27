4bcefc78432b03f7286a40d580e78ff80a512331
sbx_user1051 <sbx_user1051@169.254.67.89>
2022-05-27 14:03:24 +0000

    Fri May 27 14:03:24 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,7 @@
 # friend_name = unicode string
 def hello(friend_name):
     if type(friend_name) == str:
-        return print('Hello, World!')
+        return print(friend_name)
     else:
         raise TypeError('must pass str as argument')
+
