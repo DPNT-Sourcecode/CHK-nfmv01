66d2b918d805a8aaaf4d3da98785a828c13fe28f
sbx_user1051 <sbx_user1051@169.254.6.169>
2022-05-27 16:57:05 +0000

    Fri May 27 16:57:05 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,2 +1,9 @@
-from solutions.CHK import checkout_solution
+from lib.solutions.CHK import checkout_solution
+import unittest
+
+class TestCheckout(unittest.TestCase):
+    def test_checkout(self):
+        skus = ['A', 'A', 'B', 'A', 'D', 'C', 'B', 'A', 'A']
+        self.assertEqual(checkout_solution(skus), 310)
+
 
