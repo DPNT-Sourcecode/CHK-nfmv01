f80c97a146010bd5fd4ef3fbe014f984590453a4
sbx_user1051 <sbx_user1051@169.254.67.89>
2022-05-27 14:05:24 +0000

    Fri May 27 14:05:24 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,8 +1,8 @@
+
+
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    if type(friend_name) == str:
-        return print(friend_name)
-    else:
-        raise TypeError('must pass str as argument')
+    return str(friend_name)
+
 
