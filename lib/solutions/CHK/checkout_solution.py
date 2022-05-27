d05a0d765d5363ff8fe80388608dc287992b242d
sbx_user1051 <sbx_user1051@169.254.176.201>
2022-05-27 14:28:39 +0000

    Fri May 27 14:28:39 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if all(isinstance(x, str) for x in skus):
+        print('success')
+    else:
+        return -1
+
+
+skus = ['a', 'g', 1]
+
+checkout(skus)
 
