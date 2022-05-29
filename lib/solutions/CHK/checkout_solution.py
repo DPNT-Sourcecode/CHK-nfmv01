8a5e00d4cc175db8518cc4933163bbc8a42b5a90
sbx_user1051 <sbx_user1051@169.254.1.149>
2022-05-29 23:30:09 +0000

    Sun May 29 23:30:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
+products = {'A': [50, '3A-130', '5A-200'],
+            'B': [30, '2B-45'],
+            'C': [20],
+            'D': [15],
+            'E': [40, '2E-FREE-B'],
+            'F': [10, '2F-FREE-F'],
+            'G': [20],
+            'H': [10, '5H-45', '10H-80'],
+            }
 
 
 def checkout(skus):
@@ -12,4 +21,5 @@ def checkout(skus):
 #
 #
 # print(checkout(test_sku))
+
 
